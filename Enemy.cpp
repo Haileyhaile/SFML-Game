848b2ccd6bd50348e650b7f56a0f0f52e2b8a1aa
Haileyhaile <55165183+Haileyhaile@users.noreply.github.com>
2019-12-02 18:51:22 -0800

    Enemy class

---
+++ Enemy.cpp
@@ -0,0 +1,9 @@
+//
+//  Enemy.cpp
+//  SFML
+//
+//  Created by Hailey Le on 12/1/19.
+//  Copyright © 2019 Phuong Thao Le. All rights reserved.
+//
+
+#include "Enemy.hpp"
---
+++ Enemy.hpp
@@ -0,0 +1,16 @@
+//
+//  Enemy.hpp
+//  SFML
+//
+//  Created by Hailey Le on 12/1/19.
+//  Copyright © 2019 Phuong Thao Le. All rights reserved.
+//
+
+#ifndef Enemy_hpp
+#define Enemy_hpp
+
+#include "Commons.hpp"
+#include "Bullet.hpp"
+#include "Animation.hpp"
+
+#endif /* Enemy_hpp */
