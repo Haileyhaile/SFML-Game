35e0182bf033e38270de1bdebfd7cbb2feeb3413
Haileyhaile <55165183+Haileyhaile@users.noreply.github.com>
2019-12-02 18:49:31 -0800

    Commons headers and functions

---
+++ Commons.cpp
@@ -0,0 +1,9 @@
+//
+//  Commons.cpp
+//  SFML
+//
+//  Created by Hailey Le on 12/1/19.
+//  Copyright © 2019 Phuong Thao Le. All rights reserved.
+//
+
+#include "Commons.hpp"
---
+++ Commons.hpp
@@ -0,0 +1,22 @@
+//
+//  Commons.hpp
+//  SFML
+//
+//  Created by Hailey Le on 12/1/19.
+//  Copyright © 2019 Phuong Thao Le. All rights reserved.
+//
+
+#ifndef Commons_hpp
+#define Commons_hpp
+
+#include <stdio.h>
+#include <SFML/Graphics.hpp>
+#include <SFML/Window.hpp>
+#include <iostream>
+#include <stdlib.h>
+#include <SFML/System.hpp>
+#include <vector>
+
+using namespace sf;
+
+#endif /* Commons_hpp */
