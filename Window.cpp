135d330740dd2a44af3531540ec716e774123572
Haileyhaile <55165183+Haileyhaile@users.noreply.github.com>
2019-12-03 17:55:19 -0800

    Updates window class

--- Window.cpp
+++ Window.cpp
@@ -1,156 +1,173 @@
-#include "Window.hpp"
-using namespace std;
+//
+//  Window.cpp
+//  SFML
+//
+//  Created by Hailey Le on 12/3/19.
+//  Copyright © 2019 Phuong Thao Le. All rights reserved.
+//
 
+#include "Window.hpp"
 WindowGestion::WindowGestion(RenderWindow &startMenu, RenderWindow &game, RenderWindow &deathWindow)
 {
-	StartMenu = &startMenu;
-	Game = &game;
-	DeathWindow = &deathWindow;
+    StartMenu = &startMenu;
+    Game = &game;
+    DeathWindow = &deathWindow;
 
-	StartMenu->setFramerateLimit(60);
+    StartMenu->setFramerateLimit(60);
 
-	Game->setFramerateLimit(60);
-	Game->setVisible(false); // This window isn't show fisrt
-	Game->setActive(false); // So inactive too
+    Game->setFramerateLimit(60);
+    Game->setVisible(false); // This window isn't show fisrt
+    Game->setActive(false); // So inactive too
 
-	DeathWindow->setFramerateLimit(60);
-	DeathWindow->setVisible(false); // Same thing for the Death Window
-	DeathWindow->setActive(false);
+    DeathWindow->setFramerateLimit(60);
+    DeathWindow->setVisible(false); // Same thing for the Death Window
+    DeathWindow->setActive(false);
 }
 
 bool WindowGestion::Show(WindowType type)
 {
-	switch (type)
-	{
-	case StartWindow:
-	{
-		Game->setVisible(false);
-		Game->setActive(false);
+   
+    Font font;
+    
+    // I code this on my Mac so if you're using Microsoft or other environment
+    // you can just use is line of code
+    // font.loadFromFile("Resources/#9Slide03ArimaMaduraiMedium.tff");
+    font.loadFromFile("/Users/phuongthaolee/Projects/Resources/ArimaMaduraiMedium.ttf");
+    switch (type)
+    {
+    case StartWindow:
+    {
+        Game->setVisible(false);
+        Game->setActive(false);
 
-		DeathWindow->setVisible(false);
-		DeathWindow->setActive(false);
+        DeathWindow->setVisible(false);
+        DeathWindow->setActive(false);
 
-		StartMenu->setVisible(true);
-		StartMenu->setActive(true);
+        StartMenu->setVisible(true);
+        StartMenu->setActive(true);
 
-		Text Maintxt, Subtxt;
-		string main = "WELCOME TO SPACE SHOOTER";
-		string sub = "Press Return to Start the Game or Esc to Exit.";
+        Text Maintxt, Subtxt;
+        String main = "WELCOME TO SPACE SHOOTER";
+        String sub = "Press Return to Start the Game or Esc to Exit.";
 
-		Maintxt.setFont(font);
-		Maintxt.setFillColor(Color::White);
-		Maintxt.setCharacterSize(40);
-		Maintxt.setPosition(150.0, 150.0);
-		Maintxt.setString(main);
+        Maintxt.setFont(font);
+        Maintxt.setFillColor(Color::White);
+        Maintxt.setCharacterSize(40);
+        Maintxt.setPosition(150.0, 150.0);
+        Maintxt.setString(main);
 
-		Subtxt.setFont(font);
-		Subtxt.setFillColor(Color::White);
-		Subtxt.setCharacterSize(30);
-		Subtxt.setPosition(270.0, 220.0);
-		Subtxt.setString(sub);
+        Subtxt.setFont(font);
+        Subtxt.setFillColor(Color::White);
+        Subtxt.setCharacterSize(30);
+        Subtxt.setPosition(270.0, 220.0);
+        Subtxt.setString(sub);
+        
+        Menu menu("WELCOME TO SPACE SHOOTER", "Press Return to play\nPress Escape to quit.");
+        
+        if (menu.doMenu(menu, *StartMenu))
+            return true;
+        else
+            return false;
+    }
 
-		if (menu.doMenu(menu, *StartMenu))
-			return true;
-		else
-			return false;
-	}
+    case PlayWindow:
+    {
+        StartMenu->setVisible(false);
+        StartMenu->setActive(false);
 
-	case PlayWindow:
-	{
-		StartMenu->setVisible(false);
-		StartMenu->setActive(false);
+        DeathWindow->setVisible(false);
+        DeathWindow->setActive(false);
 
-		DeathWindow->setVisible(false);
-		DeathWindow->setActive(false);
+        Game->setVisible(true);
+        Game->setActive(true);
 
-		Game->setVisible(true);
-		Game->setActive(true);
+        return true;
+    }
 
-		return true;
-	}
+    case DeathScreenWindow:
+    {
+        Game->setVisible(false);
+        Game->setActive(false);
 
-	case DeathScreenWindow:
-	{
-		Game->setVisible(false);
-		Game->setActive(false);
+        DeathWindow->setVisible(true);
+        DeathWindow->setActive(true);
 
-		DeathWindow->setVisible(true);
-		DeathWindow->setActive(true);
+        Text Maintxt, Subtxt;
+        String main = "WELCOME TO SPACE SHOOTER";
+        String sub = "Press Return to Start the Game or Esc to Exit.";
 
-		Text Maintxt, Subtxt;
-		string main = "WELCOME TO SPACE SHOOTER";
-		string sub = "Press Return to Start the Game or Esc to Exit.";
+        Maintxt.setFont(font);
+        Maintxt.setFillColor(Color::White);
+        Maintxt.setCharacterSize(40);
+        Maintxt.setPosition(150.0, 150.0);
+        Maintxt.setString(main);
 
-		Maintxt.setFont(font);
-		Maintxt.setFillColor(Color::White);
-		Maintxt.setCharacterSize(40);
-		Maintxt.setPosition(150.0, 150.0);
-		Maintxt.setString(main);
+        Subtxt.setFont(font);
+        Subtxt.setFillColor(Color::White);
+        Subtxt.setCharacterSize(30);
+        Subtxt.setPosition(270.0, 220.0);
+        Subtxt.setString(sub);
+        
+        Menu menu("YOU LOSE!", "Press Return to play\nPress Escape to quit.");
 
-		Subtxt.setFont(font);
-		Subtxt.setFillColor(Color::White);
-		Subtxt.setCharacterSize(30);
-		Subtxt.setPosition(270.0, 220.0);
-		Subtxt.setString(sub);
+        if (menu.doMenu(menu, *DeathWindow))
+            return true;
+        else
+            return false;
+    }
 
-		if (menu.doMenu(menu, *DeathWindow))
-			return true;
-		else
-			return false;
-	}
-
-	default:
-		return true;
-	}
+    default:
+        return true;
+    }
 }
 
 RenderWindow* WindowGestion::getWindow(WindowType type)
 {
-	switch (type)
-	{
-	case StartWindow:
-		return StartMenu;
+    switch (type)
+    {
+    case StartWindow:
+        return StartMenu;
 
-	case PlayWindow:
-		return Game;
+    case PlayWindow:
+        return Game;
 
-	case DeathScreenWindow:
-		return DeathWindow;
+    case DeathScreenWindow:
+        return DeathWindow;
 
-	default:
-		return NULL;
-	}
+    default:
+        return NULL;
+    }
 }
 
 WindowGestion::~WindowGestion()
 {
-	if (StartMenu->isOpen())
-	{
-		StartMenu->close();
-		StartMenu->~RenderWindow();
-	}
-	else
-	{
-		StartMenu->~RenderWindow();
-	}
+    if (StartMenu->isOpen())
+    {
+        StartMenu->close();
+        StartMenu->~RenderWindow();
+    }
+    else
+    {
+        StartMenu->~RenderWindow();
+    }
 
-	if (Game->isOpen())
-	{
-		Game->close();
-		Game->~RenderWindow();
-	}
-	else
-	{
-		Game->~RenderWindow();
-	}
+    if (Game->isOpen())
+    {
+        Game->close();
+        Game->~RenderWindow();
+    }
+    else
+    {
+        Game->~RenderWindow();
+    }
 
-	if (DeathWindow->isOpen())
-	{
-		DeathWindow->close();
-		DeathWindow->~RenderWindow();
-	}
-	else
-	{
-		DeathWindow->~RenderWindow();
-	}
+    if (DeathWindow->isOpen())
+    {
+        DeathWindow->close();
+        DeathWindow->~RenderWindow();
+    }
+    else
+    {
+        DeathWindow->~RenderWindow();
+    }
 }
--- Window.hpp
+++ Window.hpp
@@ -1,30 +1,38 @@
-#ifndef HEADERS_WINDOW
-#define HEADERS_WINDOW
+//
+//  Window.hpp
+//  SFML
+//
+//  Created by Hailey Le on 12/3/19.
+//  Copyright © 2019 Phuong Thao Le. All rights reserved.
+//
 
-#include <SFML/Graphics.hpp>
-#include <iostream>
+#ifndef Window_hpp
+#define Window_hpp
 
-using namespace sf;
+#include "Commons.hpp"
+#include "Menu.hpp"
+#include <string>
 
 enum WindowType
 {
-	StartWindow,
-	PlayWindow,
-	DeathScreenWindow
+    StartWindow,
+    PlayWindow,
+    DeathScreenWindow
 };
 
 class WindowGestion
+
 {
 public:
-	WindowGestion(RenderWindow &startMenu, RenderWindow &game, RenderWindow &deathWindow);
-	bool Show(WindowType type);
-	RenderWindow* getWindow(WindowType type);
-	~WindowGestion();
+    WindowGestion(RenderWindow &startMenu, RenderWindow &game, RenderWindow &deathWindow);
+    bool Show(WindowType type);
+    RenderWindow* getWindow(WindowType type);
+    ~WindowGestion();
 
 private:
-	RenderWindow *StartMenu = nullptr;
-	RenderWindow *Game = nullptr;
-	RenderWindow *DeathWindow = nullptr;
+    RenderWindow *StartMenu = nullptr;
+    RenderWindow *Game = nullptr;
+    RenderWindow *DeathWindow = nullptr;
 };
 
-#endif
+#endif /* Window_hpp */
